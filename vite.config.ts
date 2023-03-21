67fc88a421225febcd9d1403b369890dc24fd235
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2023-03-21 13:35:48 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ViteRadar from "vite-plugin-radar";
+import {VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteRadar({
-    enableDev: true,
-    analytics:{
-      id:"G-5FNT24WH21"
-    }
-  })],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: "G-XXXXX",
+      },
+    }),
+  ],
   server: {
     watch: {
       usePolling: true,
